d85f89361416ed13e079d3d9f21ea45622c93803
Aditya <adityapandeyadp@gmail.com>
2025-01-04 18:17:20 +0530

    change in fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,11 @@ import Navbar from "@/components/globals/navbar";
 import { ThemeProvider } from "@/components/globals/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const font = DM_Sans({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +21,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={font.className}>
+
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
